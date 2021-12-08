1cfc46980bcf4f0f398a5a40bbcd32a93fc7619e
Jimmi is Coding <jimmicodathon@gmail.com>
2021-12-08 14:05:04 -0600

    GeneralInfo now uses state and lifts it to CVInput's state

--- src/components/CVInput.js
+++ src/components/CVInput.js
@@ -12,7 +12,7 @@ class CVInput extends Component {
   constructor(props) {
       super(props);
 
-      this.updateGeneralInfo = this.updateGeneralInfo.bind(this);
+      this.liftStateToCVInput = this.liftStateToCVInput.bind(this);
 
       this.state = {
         generalInfo: { /*
@@ -22,59 +22,35 @@ class CVInput extends Component {
           phone: 0 */
         },
         educationExp: [
-          { /*
+          /*{
             name: ,
             city: ,
             degree: ,
             startDate: ,
-            endDate: */
-          },
+            endDate:
+          },*/
         ],
         practicalExp: [
-          { /*
+          /*{
             title: ,
             name: ,
             city: ,
             startDate: ,
-            endDate: */ 
-          }
+            endDate: 
+          } */
         ],
       };
   };
 
-  updateGeneralInfo(e) {
-    console.log(this.state.generalInfo);
-    const name = e.target.name;
-    const value = e.target.value;
-    let editInfo = this.state.generalInfo;
-
-    if(name === 'first-name') {
-      editInfo.firstName = value;
-      this.setState({generalInfo: editInfo})
-    }
-    else if(name === 'last-name') {
-      editInfo.lastName = value;
-      this.setState({generalInfo: editInfo})
-    } 
-    else if(name === 'email') {
-      editInfo.email = value;
-      this.setState({generalInfo: editInfo})
-    } 
-    else if(name === 'phone') {
-      editInfo.phone = value;
-      this.setState({generalInfo: editInfo})
-    } 
-    else {
-      console.log('unkown general info being changed')
-    }
-    console.log(this.state.generalInfo)
-  }
+  liftStateToCVInput(name, value) {
+    this.setState({[name]: value});
+  };
 
   render() {
 
     return (
       <div className="cv-inputs" >
-          <GeneralInfo updateGeneralInfo={this.updateGeneralInfo} />
+          <GeneralInfo liftStateToCVInput={this.liftStateToCVInput} />
           <EducationExp />
           <PracticalExp />
       </div>
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,7 +3,39 @@ import React, { Component } from "react";
 class GeneralInfo extends Component {
   constructor(props) {
     super(props);
+
+    this.updateGeneralInfo = this.updateGeneralInfo.bind(this);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: 0 
+    }
   };
+
+  updateGeneralInfo(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if(name === 'first-name') {
+      this.setState({firstName: value})
+    }
+    else if(name === 'last-name') {
+      this.setState({lastName: value})
+    } 
+    else if(name === 'email') {
+      this.setState({email: value})
+    } 
+    else if(name === 'phone') {
+      this.setState({phone: value})
+    } 
+    else {
+      console.log('unkown general info being changed')
+    }
+    console.log("generalInfo's state", this.state);
+    this.props.liftStateToCVInput('generalInfo', this.state);
+  }
 
   render() {
 
@@ -12,19 +44,19 @@ class GeneralInfo extends Component {
         <h1>Personal Info</h1>
         <div>
             <label name="first-name">First Name</label>
-            <input onChange={this.props.updateGeneralInfo} type="text" id="first-name" name="first-name" required minLength="1"></input>
+            <input onChange={this.updateGeneralInfo} type="text" id="first-name" name="first-name" required minLength="1"></input>
         </div>
         <div>
             <label name="last-name">Last Name</label>
-            <input onChange={this.props.updateGeneralInfo} type="text" id="last-name" name="last-name" required minLength="1"></input>
+            <input onChange={this.updateGeneralInfo} type="text" id="last-name" name="last-name" required minLength="1"></input>
         </div>
         <div>
             <label name="email">Email</label>
-            <input onChange={this.props.updateGeneralInfo} type="text" id="email" name="email" required minLength="1"></input>
+            <input onChange={this.updateGeneralInfo} type="text" id="email" name="email" required minLength="1"></input>
         </div>
         <div>
             <label name="phone">Phone</label>
-            <input onChange={this.props.updateGeneralInfo} type="text" id="phone" name="phone" required minLength="1"></input>
+            <input onChange={this.updateGeneralInfo} type="text" id="phone" name="phone" required minLength="1"></input>
         </div>
       </form>
     );
