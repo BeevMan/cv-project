301fb96d2d6c6548fe19618bb5a6027ec1b03282
Jimmi is Coding <jimmicodathon@gmail.com>
2021-12-08 12:40:42 -0600

    general info saved in state onchange

--- src/components/CVInput.js
+++ src/components/CVInput.js
@@ -11,13 +11,70 @@ import PracticalExp from "./PracticalExp";
 class CVInput extends Component {
   constructor(props) {
       super(props);
+
+      this.updateGeneralInfo = this.updateGeneralInfo.bind(this);
+
+      this.state = {
+        generalInfo: { /*
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone: 0 */
+        },
+        educationExp: [
+          { /*
+            name: ,
+            city: ,
+            degree: ,
+            startDate: ,
+            endDate: */
+          },
+        ],
+        practicalExp: [
+          { /*
+            title: ,
+            name: ,
+            city: ,
+            startDate: ,
+            endDate: */ 
+          }
+        ],
+      };
   };
+
+  updateGeneralInfo(e) {
+    console.log(this.state.generalInfo);
+    const name = e.target.name;
+    const value = e.target.value;
+    let editInfo = this.state.generalInfo;
+
+    if(name === 'first-name') {
+      editInfo.firstName = value;
+      this.setState({generalInfo: editInfo})
+    }
+    else if(name === 'last-name') {
+      editInfo.lastName = value;
+      this.setState({generalInfo: editInfo})
+    } 
+    else if(name === 'email') {
+      editInfo.email = value;
+      this.setState({generalInfo: editInfo})
+    } 
+    else if(name === 'phone') {
+      editInfo.phone = value;
+      this.setState({generalInfo: editInfo})
+    } 
+    else {
+      console.log('unkown general info being changed')
+    }
+    console.log(this.state.generalInfo)
+  }
 
   render() {
 
     return (
       <div className="cv-inputs" >
-          <GeneralInfo />
+          <GeneralInfo updateGeneralInfo={this.updateGeneralInfo} />
           <EducationExp />
           <PracticalExp />
       </div>
